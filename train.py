af61b16baa01c84117716d5f83e1190b938a64b1
worl2997 <worl2997@naver.com>
2022-01-18 10:41:58 +0900

    revised

--- train.py
+++ train.py
@@ -66,11 +66,11 @@ def main(args=None):
     else:
         raise ValueError('Dataset type not understood (must be csv or coco), exiting.')
 
-    sampler = AspectRatioBasedSampler(dataset_train, batch_size=6, drop_last=False)
+    sampler = AspectRatioBasedSampler(dataset_train, batch_size=2, drop_last=False)
     dataloader_train = DataLoader(dataset_train, num_workers=3, collate_fn=collater, batch_sampler=sampler)
 
     if dataset_val is not None:
-        sampler_val = AspectRatioBasedSampler(dataset_val, batch_size=3, drop_last=False)
+        sampler_val = AspectRatioBasedSampler(dataset_val, batch_size=1, drop_last=False)
         dataloader_val = DataLoader(dataset_val, num_workers=3, collate_fn=collater, batch_sampler=sampler_val)
     # Create the model
     if parser.depth == 18:
