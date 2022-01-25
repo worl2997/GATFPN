92ad14ba2463fc7ef75ee32e0d1c4debd02224f4
worl2997 <worl2997@naver.com>
2022-01-25 14:42:29 +0900

    voc_proposed

--- train.py
+++ train.py
@@ -150,14 +150,14 @@ def main(args=None):
 
         scheduler.step(np.mean(epoch_loss))
 
-        torch.save(retinanet.module, 'weights/new_weight_{}.pt'.format(epoch_num+1))
+        torch.save(retinanet.module, 'brandnew/br_ver2_p6add_{}.pt'.format(epoch_num+1))
         # torch.save({
         #     'model_state_dict': retinanet.state_dict(),
         #     'optimizer_state_dict': optimizer.state_dict(),
         #     'loss': loss,
         # }, '{}_GCN_retinanet_{}.pt'.format(parser.dataset, epoch_num))
     retinanet.eval()
-    torch.save(retinanet, 'weights/new_weight.pt')
+    torch.save(retinanet, 'brandnew/br_ver2_p6add_final.pt')
 
 
 if __name__ == '__main__':
